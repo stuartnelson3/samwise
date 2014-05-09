1e432ada03ee5dd64507f94785a7353a4a6bd06a
Stuart Nelson <stuartnelson3@gmail.com>
2014-05-09 14:17:36 -0500

    Some more tests.

--- server.go
+++ server.go
@@ -10,17 +10,18 @@ import (
 )
 
 // write tests you lazy slob
+var c = consistent.New()
+
 func main() {
 	// load config file of servers
 	// need to keep a heartbeat on servers and remove dead ones, then add them
 	// to a queue to try to restart them.
-	c := consistent.New()
 
 	// load servers from config file into servers map
 	// should this be a slice that values are removed from instead of a map??
 
 	// servers := map[string]bool{"server1": true, "server2": true, "server3": true}
-    servers := map[string]bool{"http://localhost:9000": true}
+	servers := map[string]bool{"http://localhost:9000": true}
 	// confirm that the server are up??
 	for server, _ := range servers {
 		c.Add(server)
@@ -35,35 +36,18 @@ func main() {
 		AllowHeaders: []string{"Origin"},
 	}))
 
-	m.Get("/stream", func(w http.ResponseWriter, r *http.Request) {
-		token := r.FormValue("token")
-		if token != os.Getenv("TOKEN") {
-			return
-		}
-
-		stream := r.FormValue("stream")
-		if stream == "" {
-			return
-		}
-		// redirect to server based on consistent hash
-
-		// alternatively, have rails query this endpoint to get the route, then
-		// have rails return the found server to the client
-		server, err := c.Get(stream)
-		if err != nil {
-			return
-		}
-		http.RedirectHandler(server+"/stream?&token="+token+"stream="+stream, 302).ServeHTTP(w, r)
-	})
+	m.Get("/stream", Stream)
 
 	m.Post("/add_server", func(w http.ResponseWriter, r *http.Request) {
 		server := r.FormValue("server")
 		_, err := http.Get(server)
 		if err != nil {
 			servers[server] = false
+			w.WriteHeader(404)
 			// write unsuccessful response
 		} else {
 			servers[server] = true
+			w.WriteHeader(200)
 			// make sure this isn't a duplicate!!
 			c.Add(server)
 			// write successful response
@@ -85,6 +69,30 @@ func main() {
 		http.PostForm(server+"/update_stream", r.PostForm)
 	})
 	m.Run()
+}
+
+func Stream(w http.ResponseWriter, r *http.Request) {
+	token := r.FormValue("token")
+	if token != os.Getenv("TOKEN") {
+		w.WriteHeader(404)
+		return
+	}
+
+	stream := r.FormValue("stream")
+	if stream == "" {
+		w.WriteHeader(404)
+		return
+	}
+	// redirect to server based on consistent hash
+
+	// alternatively, have rails query this endpoint to get the route, then
+	// have rails return the found server to the client
+	server, err := c.Get(stream)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	http.RedirectHandler(server+"/stream?&token="+token+"stream="+stream, 302).ServeHTTP(w, r)
 }
 
 func MonitorServers(servers map[string]bool, c *consistent.Consistent) {
--- server_test.go
+++ server_test.go
@@ -1,48 +1,91 @@
 package main
 
 import (
-    "testing"
-    "net/http/httptest"
-    "net/http"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	// "log"
+	"os"
 )
 
 func setup(t *testing.T) *httptest.Server {
-    t.Log("Setup testing environment")
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", heartBeat)
-    return httptest.NewServer(mux)
+	t.Log("Setup testing environment")
+	os.Setenv("TOKEN", "secret")
+	c.Add("example")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", heartBeat)
+	mux.HandleFunc("/stream", Stream)
+	return httptest.NewServer(mux)
 }
 
 func heartBeat(w http.ResponseWriter, r *http.Request) {
-    if r.FormValue("token") == "secret" {
-        w.WriteHeader(200)
-    } else {
-        w.WriteHeader(404)
-    }
+	if r.FormValue("token") == "secret" {
+		w.WriteHeader(200)
+	} else {
+		w.WriteHeader(404)
+	}
 }
 
-func servers() map[string]bool {
-    return map[string]bool{"http://localhost:8080": true}
+func endpoints() map[string]bool {
+	return map[string]bool{"http://localhost:8080": true}
 }
 
-func TestStreamEndpoint(t *testing.T) {
-    server := setup(t)
-    resp, err := http.Get(server.URL + "?token=secret")
-    if err != nil {
-        t.Fail()
-    }
-    if resp.StatusCode != 200 {
-        t.Error("request unsuccessful")
-    }
+func TestHeartbeatEndpoint(t *testing.T) {
+	server := setup(t)
+	resp, err := http.Get(server.URL + "?token=secret")
+	if err != nil {
+		t.Fail()
+	}
+	if resp.StatusCode != 200 {
+		t.Error("request unsuccessful")
+	}
+}
+
+func TestHeartbeatEndpointNoToken(t *testing.T) {
+	server := setup(t)
+	resp, err := http.Get(server.URL)
+	if err != nil {
+		t.Fail()
+	}
+	if resp.StatusCode != 404 {
+		t.Error("made successful request without token")
+	}
 }
 
 func TestStreamEndpointNoToken(t *testing.T) {
-    server := setup(t)
-    resp, err := http.Get(server.URL)
-    if err != nil {
-        t.Fail()
-    }
-    if resp.StatusCode != 404 {
-        t.Error("made successful request without token")
-    }
+	server := setup(t)
+	url := server.URL + "/stream"
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fail()
+	}
+	if resp.StatusCode != 404 {
+		t.Error("stream made successful request without token")
+	}
+}
+
+func TestStreamEndpointNoStream(t *testing.T) {
+	server := setup(t)
+	url := server.URL + "/stream"
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fail()
+	}
+	if resp.StatusCode != 404 {
+		t.Error("stream made successful request without stream")
+	}
+}
+
+func TestStreamEndpoint(t *testing.T) {
+	server := setup(t)
+	url := server.URL + "/stream?token=secret&stream=stream1"
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fail()
+	}
+	if resp.StatusCode != 404 {
+		// got redirected and then 404'd
+		t.Error("did not get redirected")
+		t.Errorf("status code: %d", resp.StatusCode)
+	}
 }
